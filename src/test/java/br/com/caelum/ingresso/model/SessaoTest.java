0f7164c5e04b5ff20ea41e115ee7f40da0f4c4f2
Thais Cunha <t.cunha@samsung.com>
2018-04-17 16:46:08 -0300

    mais informacoes exercicio capitulo 3

---
+++ src/test/java/br/com/caelum/ingresso/model/SessaoTest.java
@@ -0,0 +1,22 @@
+package br.com.caelum.ingresso.model;
+
+import java.math.BigDecimal;
+import java.time.Duration;
+import java.time.LocalTime;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class SessaoTest {
+
+	@Test
+	public void oPrecoDaSessaoDeveSerIgualASomaDoPrecoDaSalaMaisPrecoDoFilme() {
+		Sala sala = new Sala ("Eldorado -IMax", new BigDecimal ("22.5"));
+		Filme filme = new Filme ("Rogue One", Duration.ofMinutes(120), "SCI-FI", new BigDecimal ("12.0"));
+		
+		BigDecimal somaDosPrecosDaSalaEFilme = sala.getPreco().add(filme.getPreco());
+		Sessao sessao = new Sessao (filme, sala, LocalTime.parse("10:00:00"));
+		Assert.assertEquals(somaDosPrecosDaSalaEFilme, sessao.getPreco());
+	}
+}
